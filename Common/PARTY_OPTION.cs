f2efad3c5ae7a2e1cbf98e44ef6e35f2fdd89977
Shane Lynch <lynch3@gmail.com>
2020-10-14 11:34:02 -0700

    Adding 1.4.8.0 source.

---
+++ Common/PARTY_OPTION.cs
@@ -0,0 +1,14 @@
+using System;
+using PartyCSharpSDK.Interop;
+
+namespace PartyCSharpSDK
+{
+    //typedef enum PARTY_OPTION
+    //{
+    //    PARTY_OPTION_LOCAL_UDP_SOCKET_BIND_ADDRESS,
+    //} PARTY_OPTION;
+    public enum PARTY_OPTION : UInt32
+    {
+        PARTY_OPTION_LOCAL_UDP_SOCKET_BIND_ADDRESS = 0,
+    }
+}
